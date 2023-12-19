cb5c8d1a62ddda28b6d6211922208b95fa6be404
matAliajni <matin.alijani@gmail.com>
2023-12-18 18:38:30 -0800

    Update many code ...

--- mth/blue_keccak256.py
+++
@@ -1,9 +0,0 @@
-
-import hashlib
-import requests
-
-page = requests.get('https://labs.nobitex.ir/')
-page_digi = page.text.encode('utf-8')
-sipi = hashlib.sha256(page_digi).hexdigest()
-bipi = hashlib.blake2b(page.text.encode('utf-8'), digest_size=64).hexdigest()
-print(sipi,'\nand blake2B:\n',bipi)
